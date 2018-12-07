9e970d8a830b49b271f8a1472937f25dfbf1469c
Yasmmin Claudino <yayamonteiro97@gmail.com>
2018-12-07 12:00:49 -0300

    att

--- 17-onibus.py
+++ 17-onibus.py
@@ -1,24 +1,22 @@
-#funções
-def buscar(lista, atual, chegada, contador = 1, listaVisitados = []):
-	if chegada in lista[atual]:
-		print(contador)
+def procura(lista, atual, destino, caminho = 1, visitados = []):
+	if destino in lista[atual]:
+		print(caminho)
 		return
-	else:
-		listaVisitados.append(atual)
-		for j in lista[atual]:
-			if j not in listaVisitados:
-				if buscar(lista, j, chegada, contador+1, listaVisitados):
-					return True
+	visitados.append(atual)
+	for i in lista[atual]:
+		if i not in visitados:
+			if procura(lista, i, destino, caminho+1, visitados):
+				return True
 	return False
 
-#main
-qtd, cOrigem, cChegada = [int(x) for x in input().split()]
-cidades = [[] for x in range(qtd)]
-for i in range(qtd-1):
-	a, b = [int(y) for y in input().split()]
-	cidades[a-1].append(b-1)
-	cidades[b-1].append(a-1)
-listaVisitados = []
-contador = 0
 
-buscar(cidades, cOrigem-1, cChegada-1)
+cidades, origem, destino = [int(c) for c in input().split()]
+listacidades = [[] for _ in range(cidades)]
+for i in range(cidades-1):
+	a, b = [int(n) for n in input().split()]
+	listacidades[a-1].append(b-1)
+	listacidades[b-1].append(a-1)
+visitados = []
+caminho = 0
+
+procura(listacidades, origem-1, destino-1)
